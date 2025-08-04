02d3da7a04c3c1517c9200ef8ec20a848b2e224f
Guilherme Marroquio <guilhermemarroquio@gmail.com>
2025-08-03 21:46:31 -0300

    add hover to button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,6 +1,6 @@
 export function Button({ children }: { children: React.ReactNode }) {
   return (
-    <button className="bg-primary font-bold text-background py-4 w-full rounded-lg">
+    <button className="bg-primary font-bold text-background py-4 w-full rounded-lg hover:bg-primary/90 cursor-pointer">
       {children}
     </button>
   );
