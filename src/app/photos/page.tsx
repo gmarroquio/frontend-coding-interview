c4b9c9c7d795725c2a6de4315199d6dc1614203e
Guilherme Marroquio <guilhermemarroquio@gmail.com>
2025-08-03 21:46:41 -0300

    fix square avg color and line height

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -23,12 +23,12 @@ export function PhotoInfo({
       <Star selected={selected} />
       <img src={img} className="w-[75px] h-[75px] rounded-lg" />
       <div className="flex w-full justify-between">
-        <div>
+        <div className="text-sm leading-none space-y-1.5">
           <p className="font-bold">{name}</p>
           <p>{alt}</p>
           <div className="flex items-center space-x-2">
-            <p>{color}</p>
-            <div className={`h-3 w-3 bg-[${color}]`} />
+            <span style={{ color }}>{color}</span>
+            <div className={"h-3 w-3"} style={{ backgroundColor: color }} />
           </div>
         </div>
         <div>
@@ -49,7 +49,7 @@ export function PhotoInfo({
 
 export default function Photos() {
   return (
-    <div className="max-w-125 py-6 md:h-screen mx-auto">
+    <div className="max-w-xs md:max-w-125 md:py-6 md:h-screen md:mx-auto">
       <div className="mb-6">
         <Logo />
       </div>
@@ -59,7 +59,7 @@ export default function Photos() {
           name={"Jane Doe"}
           alt={"Gold Hour Raindrops"}
           link={"#"}
-          color="#374824"
+          color={"#374824"}
           img="/image.png"
           selected
         />
@@ -67,7 +67,7 @@ export default function Photos() {
           name={"Jane Doe"}
           alt={"Gold Hour Raindrops"}
           link={"#"}
-          color="#374824"
+          color={"#374824"}
           img="/image.png"
           selected
         />
